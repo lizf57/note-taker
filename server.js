b109bf094461287757bb16122b8cd935c860621a
mona lisa <lizfriedman5@gmail.com>
2023-08-20 21:42:29 -0500

    changed PORT and pseduo code

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 // require() function to load express module to get access to express exports 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3001
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
 // require() function to load modules in route folders to be exported
-const apiRoutes = require('./routes/api')
-const htmlRoutes = require('./routes/html')
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// JSON and URL Encoded needed for POST and PUT requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Static middleware
 app.use(express.static('public'))
 
+// Use API routes
 app.use('/api', apiRoutes)
 app.use('/', htmlRoutes)
 
