2c1087a595b07c37833ee733cef7477136bdb154
mona lisa <lizfriedman5@gmail.com>
2023-08-20 21:39:18 -0500

    read create delete requests

--- routes/api/apiNote.js
+++ routes/api/apiNote.js
@@ -1,13 +1,27 @@
 const router = require('express').Router()
+const { createNew, deleteNote} = require('../../lib/notes');
+let { notes } = require('../../db/db');
 
 // read 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     // read the db.json file and return all saved notes as JSON
+    let saved = notes;
+    res.json(saved);
 })
 
 // create
-router.post('/api/notes', (req, res) => {
-    // receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved 
+router.post('/notes', (req, res) => {
+    // receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    req.body.id = notes.length.toString();
+    let note = createNew(req.body, notes);
+    res.json(note);
+})
+
+// delete
+router.delete('/notes/:id', async (req, res) => {
+    // read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    deleteNote(notes, req.params.id);
+    res.json(notes);
 })
 
 module.exports = router
