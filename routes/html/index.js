7f02d2380901d2f4c3ec00a33fbbb052b58c558d
mona lisa <lizfriedman5@gmail.com>
2023-08-20 21:41:47 -0500

    added get requests to html routes

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,17 +1,23 @@
 // new router object to handle requests and be exported 
 const router = require('express').Router();
 
+// path module 
+const path = require('path');
+
 // read
 router.get('/', (req, res) => {
     // home page
-})
+    res.sendFile(path.join(__dirname, '../../public/index.htmls'));
+});
 
 router.get('/notes', (req, res) => {
     // return notes.html
-})
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
 
 router.get('*', (req, res) => {
     // return index.html
-})
+    res.sendFile(path.join(__dirname, '../../public/index.html'))
+});
 
 module.exports = router
